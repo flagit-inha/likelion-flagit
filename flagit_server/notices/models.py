26f2c593c399c8699ca310ced2b27263a4d3805b
zeoueon <zeoueon129@gmail.com>
2025-08-12 18:45:25 +0900

    feat: notice models.py 구현

--- flagit_server/notices/models.py
+++ flagit_server/notices/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from crew.models import Crew
+from crew_members.models import CrewMember
 
 # Create your models here.
+class Notice(models.Model):
+    crew = models.ForeignKey(Crew, on_delete=models.CASCADE)
+    content = models.TextField()
+
+
+class NoticeReaction(models.Model):
+    notice = models.ForeignKey(Notice, on_delete=models.CASCADE)
+    crew_member = models.ForeignKey(CrewMember, on_delete=models.CASCADE)
+
+    REACTION_CHOICES = [('GOOD', 'GOOD'), ('BAD', 'BAD')]
+    reaction = models.CharField(max_length=10, choices=REACTION_CHOICES)
